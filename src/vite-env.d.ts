e7d6815c1d7f85c111a8e2c38665f679d45a0812
David Kuo <97716012+drkuo15@users.noreply.github.com>
2024-11-04 20:33:49 +0800

    ci(env): add TMDB API environment variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -8,6 +8,8 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
   readonly VITE_FIREBASE_APP_ID: string;
   readonly VITE_FIREBASE_MEASUREMENT_ID: string;
+  readonly VITE_TMDB_API_KEY: string;
+  readonly VITE_TMDB_API_ACCESS_TOKEN: string;
 }
 
 interface ImportMeta {
