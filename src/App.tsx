f6dfbd528cb6faff808bb179bccb69a432c95cd8
David Kuo <97716012+drkuo15@users.noreply.github.com>
2024-11-06 14:20:24 +0800

    refactor(pages): rename folder from Home to tmp

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout';
-import Home from './pages/home';
+import Home from './pages/tmp';
 import Movie from './pages/movieDetails';
 
 function App() {
